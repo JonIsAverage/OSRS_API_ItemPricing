1a215e7fc40fc8c6b4462f744c86fe6515bd9a21
JonIsAverage <JonIsAverage>
2023-11-27 16:40:05 +0000

    Changed Table Creation Logic
    Changed the delta table logic from using an overwrite spark function to creating a temp view using spark dataframe and then using the MERGE INTO function to insert/update/delete based on match criteria.

--- Notebooks/OSRS_API.py
+++ Notebooks/OSRS_API.py
@@ -59,9 +59,56 @@ spark_df_itemprice = spark.createDataFrame(df_itemprice)
 spark_df_items = spark.createDataFrame(df_items)
 del df_itemprice
 del df_items
-#Create table in metastore
-spark_df_itemprice.write.mode("overwrite").saveAsTable("default.item_prices")
-spark_df_items.write.mode("overwrite").saveAsTable("default.item_info")
+#Create temp view from spark dataframe
+spark_df_itemprice.createOrReplaceTempView("tmp_itemprice")
+spark_df_items.createOrReplaceTempView("tmp_items")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC --Insert or update item information
+# MAGIC MERGE INTO default.item_info USING tmp_items
+# MAGIC ON default.item_info.ItemID = tmp_items.ItemID
+# MAGIC WHEN MATCHED THEN
+# MAGIC   UPDATE SET
+# MAGIC     ItemID = tmp_items.ItemID,
+# MAGIC     ItemName = tmp_items.ItemName
+# MAGIC WHEN NOT MATCHED
+# MAGIC   THEN INSERT (
+# MAGIC     ItemID,
+# MAGIC     ItemName
+# MAGIC   )
+# MAGIC   VALUES (
+# MAGIC     tmp_items.ItemID,
+# MAGIC     tmp_items.ItemName
+# MAGIC   );
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC   --Insert or delete item prices based on a match from source to destination table
+# MAGIC   MERGE INTO default.item_prices USING tmp_itemprice
+# MAGIC   ON default.item_prices.ScanID = tmp_itemprice.ScanID
+# MAGIC   WHEN MATCHED THEN DELETE
+# MAGIC   WHEN NOT MATCHED
+# MAGIC   THEN INSERT (
+# MAGIC     ScanID,
+# MAGIC     ItemID,
+# MAGIC     HighPrice,
+# MAGIC     HighTime,
+# MAGIC     LowPrice,
+# MAGIC     LowTime,
+# MAGIC     ScanTime
+# MAGIC   )
+# MAGIC   VALUES (
+# MAGIC     tmp_itemprice.ScanID,
+# MAGIC     tmp_itemprice.ItemID,
+# MAGIC     tmp_itemprice.HighPrice,
+# MAGIC     tmp_itemprice.HighTime,
+# MAGIC     tmp_itemprice.LowPrice,
+# MAGIC     tmp_itemprice.LowTime,
+# MAGIC     tmp_itemprice.ScanTime
+# MAGIC   );
 
 # COMMAND ----------
 
--- Notebooks/OSRS_Playground.sql
+++ Notebooks/OSRS_Playground.sql
@@ -1,3 +1,12 @@
 -- Databricks notebook source
 --DROP TABLE default.item_prices;
 --DROP TABLE default.item_info
+
+
+-- COMMAND ----------
+
+select count(*) from default.item_prices;
+
+-- COMMAND ----------
+
+select count(*) from default.item_info;
