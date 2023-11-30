5af3f1dcbf6c16bb77c618f6eb51fc4066c896be
JonIsAverage <JonIsAverage>
2023-11-30 17:31:37 +0000

    Replace Pandas Python with Pyspark Code
    Refactored using Pyspark dataframes instead of pandas python for improved performance.

--- Notebooks/OSRS_API.py
+++ Notebooks/OSRS_API.py
@@ -1,67 +1,49 @@
 # Databricks notebook source
 import requests, json, pandas as pd, time
+import pyspark.pandas as ps
+from pyspark.sql.functions import from_json, col, explode, lit
 #URL Variables
 urlPrice = 'https://prices.runescape.wiki/api/v1/osrs/latest'
 urlItem = 'https://oldschool.runescape.wiki/?title=Module:GEIDs/data.json&action=raw&ctype=application%2Fjson'
 
 # COMMAND ----------
 
-page = requests.get(urlItem)
-data = json.loads(page.text)
-df = pd.DataFrame(data.items())
-#Remove first two lines
-df = df.iloc[2:]
-df.columns = ['ItemName','ItemID']
-df_items = df
-del df
+response = requests.get(urlItem)
+json_data = response.json()
+popped = json_data.pop('%LAST_UPDATE%')
+popped = json_data.pop('%LAST_UPDATE_F%')
+df_items = spark.createDataFrame(list(json_data.items()), ["ItemName", "ItemID"]).selectExpr("CAST(ItemID AS INT)", "ItemName")
 
 # COMMAND ----------
 
-page = requests.get(urlPrice)
+response = requests.get(urlPrice)
+data = response.json()
+item_data = data.get('data', {})
+ts_epoch = int(time.time())
 
-df_itemprice = pd.DataFrame(columns=['ScanID','ItemID','HighPrice','HighTime','LowPrice','LowTime','ScanTime'])
-
-lst = []
-
-for row in df_items.itertuples():
-    ItemID=row[0]
-    ts_epoch = int(time.time())
-    ScanID = str(ItemID) + str(ts_epoch)
-    HighPrice = ''
-    HighTime = ''
-    LowPrice = ''
-    LowTime = ''
-
-    json_data = page.json()
-    value = json_data.get('data')
-    value = value.get(str(ItemID), "empty")
-
-    if value == 'empty':
-        HighPrice = 'null'
-        HighTime = 'null'
-        LowPrice = 'null'
-        LowTime = 'null'
-    else:
-        fullinfo = json_data['data'][str(ItemID)]
-        HighPrice = str(fullinfo['high'])
-        HighTime = str(fullinfo['highTime'])
-        LowPrice = str(fullinfo['low'])
-        LowTime = str(fullinfo['lowTime'])
-        
-        lst.append({'ScanID': ScanID, 'ItemID': ItemID, 'HighPrice': HighPrice, 'HighTime': HighTime, 'LowPrice': LowPrice, 'LowTime': LowTime, 'ScanTime': ts_epoch})
-df_itemprice = pd.DataFrame(lst, columns=['ScanID','ItemID','HighPrice','HighTime','LowPrice','LowTime','ScanTime'])
+# Convert the data to a Spark DataFrame
+df_itemprice = spark.createDataFrame(
+    [(item_id, item.get("high", 0), item.get("highTime", 0), item.get("low", 0), item.get("lowTime", 0)) for item_id, item in item_data.items()],
+    ["id","high", "highTime", "low", "lowTime"]
+)
+df_itemprice = df_itemprice.withColumn("ScanTime", lit(ts_epoch))
 
 # COMMAND ----------
 
-#Write the data to a table
-#Create as spark dataframe
-spark_df_itemprice = spark.createDataFrame(df_itemprice)
-spark_df_items = spark.createDataFrame(df_items)
-del df_itemprice
-del df_items
-#Create temp view from spark dataframe
-spark_df_itemprice.createOrReplaceTempView("tmp_itemprice")
-spark_df_items.createOrReplaceTempView("tmp_items")
+df_itemprice.createOrReplaceTempView("tmp_itemprice_n")
+df_items.createOrReplaceTempView("tmp_items")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE TEMPORARY VIEW tmp_itemprice AS
+# MAGIC SELECT CONCAT(id, ScanTime) as ScanID,
+# MAGIC     id as ItemID,
+# MAGIC     high as HighPrice,
+# MAGIC     highTime as HighTime,
+# MAGIC     low as LowPrice,
+# MAGIC     lowtime LowTime,
+# MAGIC     ScanTime FROM tmp_itemprice_n;
 
 # COMMAND ----------
 
